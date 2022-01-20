b9f676b5562972fcb4339565de9f76120370457a
JulesText <32991504+JulesText@users.noreply.github.com>
2022-01-20 23:44:38 +1100

    Update test5.php

--- test5.php
+++ test5.php
@@ -11,7 +11,6 @@ error_log
 error_log_api
 db_example.php
 db_install.sql
-db_functions.php (merge 2)
 system_test.php (error_checks)
 account_functions.php
 analysis_functions.php
@@ -21,11 +20,9 @@ investment_functions.php
 order_functions.php
 price_functions.php
 tactic_functions.php
-draft_code.php (merge test1/2?)
 system_functions.php
 system_includes.php
 draft_index.php
-price_process.php (merge history/recent)
 investment_process.php
 tactic_ext_process.php
 tactic_process.php
@@ -34,3 +31,8 @@ system_performance.php
 draft_analysis.php
 analysis_process.php
 content_process.php
+
+db_functions.php (merge 2)
+price_process.php (merge history/recent)
+
+draft_code.php (update db.sql & ref)
