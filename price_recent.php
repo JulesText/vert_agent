1dc9051d37b932711ba9cd7541250722d844d723
JulesText <32991504+JulesText@users.noreply.github.com>
2022-01-20 23:40:40 +1100

    Update price_recent.php

--- price_recent.php
+++ price_recent.php
@@ -2,10 +2,13 @@
 
 include('includes.php');
 
-if(isset($_GET['pair_id'])) $pair_id = $_GET['pair_id'];
+if (isset($_GET['pair_id'])) $pair_id = $_GET['pair_id'];
 else $pair_id = FALSE;
+if (isset($_GET['history'])) $history = $_GET['history'];
+else $history = FALSE;
 
-$response = price_recent($config, $pair_id);
+if ($history) $response = price_history($config, $pair_id);
+else $response = price_recent($config, $pair_id);
 process($response, $config);
 
 include('system_metrics.php');
